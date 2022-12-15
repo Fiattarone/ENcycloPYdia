60c26651599b72529107d384cbc92512abe32a59
Fiattarone <Fiattarone@me.com>
2022-12-15 11:06:39 -0800

    Edit comment plan, verify JSON

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
+import json
 
 
 """
@@ -16,26 +17,31 @@ Need to read words.txt line by line and compile a JSON that contains:
 {
     "words": [
         {
-            "word":
-            {
-                definition: "",
-                synonyms: "",
-                antonyms: "",
-                topics: "",
-            }    
+            "word": "",
+            definition: "",
+            synonyms: "",
+            antonyms: "",
+            topics: "",
         },...
     ]
 }
 
-1. Check JSON to see if all words in words_backup.txt are present
 
-2. Read text file line by line
+
+1. Check words.txt to see if there are words remaining
+
+2. If there are, read text file line by line
     A) While line is read, Google search for word
+    B) try/except to see if we hit a 429 rate limit
     B) If dictionary entry exists, record to json
     C) Delete word from words.txt
 
 """
 
+"""
+Export CSV from DoorLoop
+Run thru settings and config
+"""
 
 if __name__ == '__main__':
     print('Welcome to ENcycloPYdia.')
@@ -48,8 +54,39 @@ if __name__ == '__main__':
 
     driver = webdriver.Chrome(options=options)
 
-    file1 = open('words.txt', 'r')
-    count = 0
+    with open("words.txt", "r") as f:
+        words = [line.rstrip() for line in f]
 
-    driver.get("https://www.google.com")
-    print(driver.find_element(By.TAG_NAME, "p").text)
+    try:
+        j = open('ENcycloPYdia.json')
+        enpy = json.load(j)
+    except json.decoder.JSONDecodeError:
+        # empty JSON, lets fill:
+        enpy = json.dumps({"words": []}, indent=4)
+        with open("ENcycloPYdia.json", "w") as outfile:
+            outfile.write(enpy)
+
+    last_entry = ""
+    try:
+        if len(enpy["words"]) == 0:
+            print("No words have been recorded.")
+        else:
+            for entry in enpy["words"]:
+                print(f'Our entry is: {entry}')
+            last_entry = enpy["words"][-1]
+    finally:
+        print("Finished reading JSON.")
+
+    # try:
+    #     for word in words:
+    #         # check to see if the word is already in the json
+    #         if word not in enpy["words"]:
+    #             print(f"Attempting to grab definition of {word}")
+    #             # driver.get("https://www.google.com")
+    #             # print(driver.find_element(By.TAG_NAME, "p").text)
+    # finally:
+    #     print("Finished running words.")
+
+
+    # driver.get("https://www.google.com")
+    # print(driver.find_element(By.TAG_NAME, "p").text)
